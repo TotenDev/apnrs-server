a15cb48518c4e7c0b073607738f73c5b6735ec51
Gabriel Pacheco <gabriel@totendev.com.br>
2012-10-23 20:19:19 -0200

    started log middleman

--- index.js
+++ index.js
@@ -7,4 +7,12 @@
 
 
 var APNRestServer = require('./lib/server.js');
+var restify = require('restify');
 APNRestServer({clientSecretUser:"clientOI",serverSecretUser:"serverIO" ,commonSecretPass:"mano"});
+
+var basicAuthClient = "Basic " + new Buffer("clientOI:mano").toString('base64');
+var basicAuthServer = "Basic " + new Buffer("serverIO:mano").toString('base64');
+var client1 = restify.createJsonClient({ url: 'http://127.0.0.1:8080/list/devices', headers: { 'Authorization':basicAuthClient,'Accept':"application/json",'Content-Type':"application/json" }});
+client1.post("/regist",{ hello: 'world' },function (err,req,res,obj) {
+  console.log(err," -- ");
+});
