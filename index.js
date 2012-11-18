4b1593dce5d7727bb8226c6d45c0163cc50bd96e
Gabriel <gabriel@totendev.com>
2012-11-18 01:30:41 -0200

    device register done

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var basicAuthServer = "Basic " + new Buffer("serverIO:man").toString('base64');
 //
 var restify = require('restify');
 var client1 = restify.createJsonClient({ url: 'http://127.0.0.1:8080/list/devices', headers: { 'Authorization':basicAuthClient,'Accept':"application/json",'Content-Type':"application/json" }});
-client1.post("/register",{ hello: 'world' },function (err,req,res,obj) {
-  console.log("ERRORRRR:",err," -- ",res.statusCode);
+client1.post("/register",{ token: 'pa',tags:["macarrao","marron"] },function (err,req,res,obj) {
+  console.log('%d -> %j', res.statusCode, res.headers);
+  console.log('%j', obj);
 });
