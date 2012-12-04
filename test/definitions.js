d2c03e9953b65b71ec72f08334762b346569f50d
Gabriel Pacheco <gabriel@totendev.com.br>
2012-12-04 20:59:11 -0200

    change db credentials

--- test/definitions.js
+++ test/definitions.js
@@ -1,5 +1,5 @@
 module.exports = function definitions () {
-  this.serverOptions = {clientSecretUser:"clientUser",serverSecretUser:"clientServer" ,commonSecretPass:"commonPass",database:{host:'localhost',user:'root',password:'root',database:'apnrs'},push:{cert:"oi.pem",key:"de"}};
+  this.serverOptions = {clientSecretUser:"clientUser",serverSecretUser:"clientServer" ,commonSecretPass:"commonPass",database:{host:'localhost',user:'root',password:'',database:'apnrs'},push:{cert:"oi.pem",key:"de"}};
   this.basicAuthClient = "Basic " + new Buffer("clientUser:commonPass").toString('base64');
   this.basicAuthServer = "Basic " + new Buffer("clientServer:commonPass").toString('base64');
   return this;
